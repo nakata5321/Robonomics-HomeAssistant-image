86c2f3a4390a003b3496d6e5c24ee37955179c7b
nakata5321 <makar5321@gmail.com>
2023-03-17 12:01:31 +0700

    change z2m to homeassistant

--- robonomics-stage/02-zigbee-mqtt/02-run.sh
+++ robonomics-stage/02-zigbee-mqtt/02-run.sh
@@ -3,10 +3,10 @@
 on_chroot << EOF
 
   install -d /opt/zigbee2mqtt
-  chown -R ${FIRST_USER_NAME}: /opt/zigbee2mqtt
+  chown -R homeassistant: /opt/zigbee2mqtt
 
-  su ${FIRST_USER_NAME} -c "git clone --depth 1 --branch 1.28.4 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt"
+  su homeassistant -c "git clone --depth 1 --branch 1.28.4 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt"
 
   cd /opt/zigbee2mqtt
-  su ${FIRST_USER_NAME} -c "npm ci"
+  su homeassistant -c "npm ci"
 EOF
