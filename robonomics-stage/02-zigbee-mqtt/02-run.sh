fe8fa55de014827935bc50bb786d223bba15d20d
nakata5321 <makar5321@gmail.com>
2023-05-26 11:29:28 +0300

    update z2m version to 1.30.4

--- robonomics-stage/02-zigbee-mqtt/02-run.sh
+++ robonomics-stage/02-zigbee-mqtt/02-run.sh
@@ -5,7 +5,7 @@ on_chroot << EOF
   install -d /opt/zigbee2mqtt
   chown -R homeassistant: /opt/zigbee2mqtt
 
-  su homeassistant -c "git clone --depth 1 --branch 1.28.4 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt"
+  su homeassistant -c "git clone --depth 1 --branch 1.30.4 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt"
 
   cd /opt/zigbee2mqtt
   su homeassistant -c "npm ci"
