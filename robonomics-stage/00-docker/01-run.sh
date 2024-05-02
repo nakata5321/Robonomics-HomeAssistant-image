b55872262069450fd63909820e4dade9db6b7311
nakata5321 <makar5321@gmail.com>
2024-05-02 15:55:59 +0300

    manually provide name

--- robonomics-stage/00-docker/01-run.sh
+++ robonomics-stage/00-docker/01-run.sh
@@ -8,7 +8,7 @@ on_chroot << EOF
   chmod a+r /etc/apt/keyrings/docker.asc
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  bookworm stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   apt-get update
