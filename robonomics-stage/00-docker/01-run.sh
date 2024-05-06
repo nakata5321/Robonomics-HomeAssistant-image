c80effa52923c14632c82ec1d980f5b6778766e5
nakata5321 <makar5321@gmail.com>
2024-05-06 14:47:56 +0300

    change arch

--- robonomics-stage/00-docker/01-run.sh
+++ robonomics-stage/00-docker/01-run.sh
@@ -7,7 +7,7 @@ on_chroot << EOF
 
   chmod a+r /etc/apt/keyrings/docker.asc
   echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  "deb [arch=arm64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   bookworm stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
